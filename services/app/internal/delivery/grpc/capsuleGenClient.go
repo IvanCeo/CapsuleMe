806277cbfda3242b913ffa9af5d9e488f8609ff4
Ivan <ceo@ivan-victorovich.ru>
2026-03-13 00:26:09 +0300

    some funcs, fixes

--- services/app/internal/delivery/grpc/capsuleGenClient.go
+++ services/app/internal/delivery/grpc/capsuleGenClient.go
@@ -6,32 +6,42 @@ import (
 	"context"
 	"errors"
 	"io"
+	"sync/atomic"
 
 	"google.golang.org/grpc"
 )
 
 // это то, откуда воркеры берут работу, id надо для индентификации запроса, инкремент
 type job struct {
-	id  int
+	id  int32
 	req *catalog.IncomingFeature
 	ctx context.Context
 }
 
 type result struct {
-	id   int
+	id   int32
 	resp *catalog.Capsule
 	err  error
 }
 
 type grpcCatalogClient struct {
-	workers int
-	client  capsulegen.CapsuleGenServiceClient
-	jobs    chan job
-	results chan result
+	workers   int
+	increment func() int32
+	client    capsulegen.CapsuleGenServiceClient
+	jobs      chan job
+	results   chan result
+}
+
+func inc() func() int32 {
+	var i atomic.Int32
+	return func() int32 {
+		i.Add(1)
+		return i.Load()
+	}
 }
 
 // доделать
-func NewGrpcCatalogClient(maxWorkers int, baseURL string, conn *grpc.ClientConn) *grpcCatalogClient {
+func NewGrpcCatalogClient(maxWorkers int, baseURL string) *grpcCatalogClient {
 	conn, err := grpc.NewClient(baseURL) // DialOptions сделать потом, они идут как второй аргумент ...DialOptions
 	if err != nil {
 		return nil
@@ -44,27 +54,27 @@ func NewGrpcCatalogClient(maxWorkers int, baseURL string, conn *grpc.ClientConn)
 		return nil
 	}
 
+	increment := inc()
+
 	return &grpcCatalogClient{
-		client:  client,
-		jobs:    jobs,
-		results: results,
-		workers: maxWorkers,
+		client:    client,
+		jobs:      jobs,
+		results:   results,
+		workers:   maxWorkers,
+		increment: increment,
 	}
 }
 
 // заводит воркеров
 func (cc *grpcCatalogClient) Start() {
-	for i := 0; i <= cc.workers; i++ {
+	for i := 0; i < cc.workers; i++ {
 		go func() {
 			for j := range cc.jobs {
 				select {
 				case <-j.ctx.Done():
-					return
+					continue // делаем так, чтобы воркер не умер, если надо одно задание пропустить
 				default:
 					res, err := cc.recommend(j.ctx, j.req)
-					if err != nil {
-						cc.results <- result{id: j.id, err: err}
-					}
 					cc.results <- result{id: j.id, resp: res, err: err}
 				}
 			}
@@ -76,7 +86,15 @@ func (cc *grpcCatalogClient) Start() {
 func toDTO(in *catalog.IncomingFeature) (*capsulegen.CapsuleGenerateRequest, error)
 func fromDTO(out *capsulegen.Capsule) (*catalog.Capsule, error)
 
-// данный метод будет просто синхронным, он будет использоваться просто для работы воркера
+func (cc *grpcCatalogClient) submit(job job) bool {
+	select {
+	case cc.jobs <- job:
+		// надо как-то сделать так, что бы не записать в закрытый канал
+		return true
+	default:
+		return false
+	}
+}
 func (cc *grpcCatalogClient) recommend(ctx context.Context, f *catalog.IncomingFeature) (*catalog.Capsule, error) {
 	dto, err := toDTO(f)
 	if err != nil {
@@ -122,7 +140,27 @@ func (cc *grpcCatalogClient) recommend(ctx context.Context, f *catalog.IncomingF
 	if err != nil {
 		return nil, err
 	}
+	res.Image = imageData
 	return res, nil
 }
 
-// в самом методе основном, который будет исполнять интерфейс, он просто достает из канала результатов нужный результат
+func (cc *grpcCatalogClient) Recommend(ctx context.Context, f *catalog.IncomingFeature) (*catalog.Capsule, error) {
+	job := job{
+		id:  cc.increment(),
+		req: f,
+		ctx: ctx,
+	}
+
+	if ok := cc.submit(job); !ok {
+		return nil, errors.New("job не положилась в канал")
+	}
+
+	// всетаки оно читает все и выбрасывает неподходящие, надо иначе организовать
+	for result := range cc.results {
+		if result.id == job.id {
+			return result.resp, result.err
+		}
+	}
+
+	return nil, errors.New("not found")
+}
